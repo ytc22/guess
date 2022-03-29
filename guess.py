3c5030f696a52e7c0b9bb3ef20f9b66599a32baf
Rosie Chen <4everstayhungry@gmail.com>
2022-03-29 18:17:53 +0800

    add count

--- guess.py
+++ guess.py
@@ -1,33 +1,23 @@
-# test
-import random
-r = random.randint(1, 100)
-# "." 是 "的"的意思
-# randint = random int產生隨機整數
-# (start, end)數值，且包含本數
-while True:
-    ans = input('請輸入數字：') # input
-    ans = int(ans)
-    if ans == r:
-        print('恭喜答對！')
-        break
-    elif ans > r:
-        print('答案比這個數字小哦!')
-    elif ans < r:
-        print('答案比這個數字大哦!')
-
-
-# ans
 import random
 # 都有空格 why
 r = random.randint(1, 100)
+count = 0 #要用一個計數基礎
 while True:
+	# 且count = 0 不能寫在while內，因為每重複一次就等於把計數歸零
+	count += 1 # count = count + 1
     num = input('請猜數字：') # num = number縮寫
     num = int(num) # casting 型別轉換
     # 因為input會存成字串，要改整數
     if num == r:
         print('你猜中了！')
+        # try: print('你猜了', count,'次') 重複性太高，不好
         break
     elif num > r:
         print('比答案大')
+        # try: print('你猜了', count,'次')
     elif num < r:
         print('比答案小')
+        # try: print('你猜了', count,'次')
+    print('這是你猜的第', count,'次')
+    # 但這樣猜中的時候就不會說猜幾次了!因為猜中下面馬上是break
+    # 若要猜中也印出，就在猜中print下面加一句
